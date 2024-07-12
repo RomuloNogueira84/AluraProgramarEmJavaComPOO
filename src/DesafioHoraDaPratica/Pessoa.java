4c0f598e75340b196034c74f221494b531cc7c13
Romulo Nogueira de Souza <romulonogueira929@gmail.com>
2024-07-12 13:22:57 -0300

    Desafio hora da pratica

--- src/DesafioHoraDaPratica/Pessoa.java
+++ src/DesafioHoraDaPratica/Pessoa.java
@@ -1,2 +1,7 @@
-package DesafioHoraDaPratica;public class Pessoa {
+package DesafioHoraDaPratica;
+//Crie uma classe Pessoa com um método que exibe "Olá, mundo!" no console.
+public class Pessoa {
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
+    }
 }
